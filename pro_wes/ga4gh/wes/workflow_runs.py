e4f9eb7a0d899f8314e4dc126906db5a82f024d8
Javed Habib <jh4official@gmail.com>
2025-01-02 17:57:15 +0530

    change the default url in another PR.

--- pro_wes/ga4gh/wes/workflow_runs.py
+++ pro_wes/ga4gh/wes/workflow_runs.py
@@ -85,7 +85,7 @@ class WorkflowRuns:
 
         # get and attach suitable WES endpoint
         document.wes_endpoint = WesEndpoint(
-            host="https://wes-na.cloud.e-infra.cz",
+            host="https://csc-wes-noauth.rahtiapp.fi",
         )
 
         # get and attach workflow run owner
